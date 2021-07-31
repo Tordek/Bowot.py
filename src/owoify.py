c94e6e982e5aad5b713ac330ae68e9aada6bfd25
Guillermo O. «Tordek» Freschi <kedrot@gmail.com>
2021-07-30 21:42:29 -0300

    Mowore

--- src/owoify.py
+++ src/owoify.py
@@ -38,6 +38,18 @@ def kaomoji(mood):
     return inner
 
 
+def matchcase(replacement):
+    def inner(match):
+        word = match.group(0)
+
+        if word.istitle():
+            return replacement.title()
+
+        chance = len([c for c in word if c.isupper()]) / len(word)
+        return ''.join(c.upper() if random.random() < chance else c for c in replacement)
+    return inner
+
+
 def owoify(text):
     text = re.sub(r'o',
                   lambda _: r'owo' if random.random() < OWO_CHANCE else 'o', text)
@@ -46,16 +58,17 @@ def owoify(text):
     text = re.sub(r'([eo][r](s?))\b',
                   lambda match: random.choice(['ah' + match.group(2), 'uh' + match.group(2), match.group(1)]), text)
     text = re.sub(r'\blmao\b', r'lmeow', text)
-    text = re.sub(r'l|r', r'w', text)
-    text = re.sub(r'L|R', r'W', text)
-    text = re.sub(r'ttl', r'ddl', text)
-    text = re.sub(r'\bthe\b', r'da', text)
-    text = re.sub(r'\bThe\b', r'Da', text)
-    text = re.sub(r'\bTHE\b', r'DA', text)
-    text = re.sub(r'\bwith\b', r'wif', text)
-    text = re.sub(r'ome', r'um', text)
-    text = re.sub(r'\bone(s?)\b', r'wun\1', text)
-    text = re.sub(r'tion', r'shun', text)
+    text = re.sub(r'l|r', r'w', text, flags=re.IGNORECASE)
+    text = re.sub(r'L|R', r'W', text, flags=re.IGNORECASE)    
+    text = re.sub(r'\bcum\b', matchcase(r'cummies'), text, flags=re.IGNORECASE)
+    text = re.sub(r'\bthe\b', matchcase(r'da'), text, flags=re.IGNORECASE)
+    text = re.sub(r'\bwith\b', matchcase(r'wif'), text, flags=re.IGNORECASE)    
+    text = re.sub(r'\bone\b', matchcase(r'wun'), text, flags=re.IGNORECASE)
+    text = re.sub(r'\bones\b', matchcase(r'wuns'), text, flags=re.IGNORECASE)
+    text = re.sub(r'\bgive\b', matchcase(r'gib'), text, flags=re.IGNORECASE)
+    text = re.sub(r'ttl', matchcase(r'ddl'), text, flags=re.IGNORECASE)
+    text = re.sub(r'tion', matchcase(r'shun'), text, flags=re.IGNORECASE)
+    text = re.sub(r'ome', matchcase(r'um'), text, flags=re.IGNORECASE)
     text = re.sub(r'([Nn])([aou])', r'\1y\2', text)
     text = re.sub(r'N([AOU])', r'NY\1', text)
     text = re.sub(r'ove', r'uv', text)
